ca936f100c8a2eec97dad504b6e9de254de88c2b
SA HU <fbshahadatislam@gmail.com>
2025-10-16 00:29:15 +0600

    Delete includes/database/models/modelAntiSt.js

--- includes/database/models/modelAntiSt.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const AntiStSchema = new mongoose.Schema({
-  threadID: { type: String, required: true, unique: true },
-  data: { type: Object, default: {} }
-});
-
-module.exports = mongoose.models.modelAntiSt || mongoose.model("modelAntiSt", AntiStSchema);
